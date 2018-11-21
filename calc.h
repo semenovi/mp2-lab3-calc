0bc5adc43751e4f39959e784924e7ce21cbabc96
semenovi <t79601712352@gmail.com>
2018-11-21 09:40:37 +0300

    Update calc.h

--- calc.h
+++ calc.h
@@ -2,49 +2,66 @@
 #define CALC
 
 #include "container.h"
-class calc
+#include <math.h>
+class preprocessor
 {
-private:
-	int priority(unit v)
+	std::string input;
+	queue postfix;
+	queue prefix;
+	// returns 0 if all right
+	int check_brackets()
 	{
-		if (v.s_value == "+")
-			return 1;
-		else if (v.s_value == "-")
-			return 1;
-		else if (v.s_value == "*")
-			return 2;
-		else if (v.s_value == "/")
-			return 2;
-		else if (v.s_value == "(")
-			return 0;
-		else if (v.s_value == ")")
-			return 0;
-		return -1;
-	}
-	bool check_brackets()
-	{
-		int c = 0;
+		int c = 0, last = 0;
 		for (int i = 0; i < input.length(); i++)
 		{
 			if (input[i] == '(')
+			{
 				c++;
+				last = i;
+			}
 			else if (input[i] == ')')
+			{
 				c--;
-			if (c < 0) return false;
+			}
+			if (c < 0) return i;
 		}
-		if (c == 0) return true;
-		return false;
+		if (c == 0) return 0;
+		return last;
 	}
-	queue to_lexems()
+	void unary_minus_to_binary()
 	{
-		if (!check_brackets()) throw (-3);
+		if (input[0] == '-')
 		{
-			unit a(input[0]);
-			std::cout << priority(a);
-			if (priority(a) > 0) throw (-5);
+			input.resize(input.length() + 1);
+			input[input.length()] = input[input.length() - 1];
+			for (int j = input.length() - 1; j >= 0; j--)
+			{
+				input[j + 1] = input[j];
+			}
+			input[0] = '0';
+
 		}
-		queue output(input.length());
-		unit current_double(0.0), current_string("");
+		for (int i = 0; i < input.length(); i++)
+		{
+			if (input[i] == '(' && input[i + 1] == '-')
+			{
+				input.resize(input.length() + 1);
+				input[input.length()] = input[input.length() - 1];
+				for (int j = input.length() - 2; j >= i; j--)
+				{
+					input[j + 1] = input[j];
+				}
+				input[i + 1] = '0';
+			}
+		}
+	}
+	queue create_postfix()
+	{
+		queue output(input.length() + 1);
+		lexem current_double(0.0), current_double_after_dot(0.0), current_string("");
+		int mode = 0;
+		if (!((int)input[0] <= (int)'9' && (int)input[0] >= (int)'0'))
+			current_string.s_value.append(&input[0], 1);
 		for (int i = 0; i < input.length(); i++)
 		{
 			// integers
@@ -59,20 +76,99 @@ private:
 						current_string.s_value.clear();
 						current_string.s_value.append(&input[i], 1);
 					}
-					current_double.d_value = current_double.d_value * 10.0;
-					current_double.d_value += ((int)input[i] - (int)'0');
-					output.push(current_double);
-					current_double.d_value = 0.0;
-					current_string.s_value.append(&input[i + 1], 1);
+					if (mode == 0)
+					{
+						current_double.d_value = current_double.d_value * 10.0;
+						current_double.d_value += ((int)input[i] - (int)'0');
+						if (input[i + 1] != '.')
+							output.push(current_double);
+					}
+					else
+					{
+						current_double_after_dot.d_value = current_double_after_dot.d_value * 10.0;
+						current_double_after_dot.d_value += ((int)input[i] - (int)'0');
+						while (current_double_after_dot.d_value > 1)
+							current_double_after_dot.d_value = current_double_after_dot.d_value / 10;
+						current_double.d_value += current_double_after_dot.d_value;
+						output.push(current_double);
+					}
+					if (input[i + 1] != '.')
+					{
+						current_double.d_value = 0.0;
+						mode = 0;
+						current_string.s_value.append(&input[i + 1], 1);
+					}
 				}
 				else
 				{
-					current_double.d_value = current_double.d_value * 10.0;
-					current_double.d_value += ((int)input[i] - (int)'0');
+					if (mode == 0)
+					{
+						current_double.d_value = current_double.d_value * 10.0;
+						current_double.d_value += ((int)input[i] - (int)'0');
+					}
+					else
+					{
+						current_double_after_dot.d_value = current_double_after_dot.d_value * 10.0;
+						current_double_after_dot.d_value += ((int)input[i] - (int)'0');
+					}
 				}
 			}
+			// dot
+			else if (input[i] == '.')
+			{
+				mode = 1;
+			}
+			// variable
+			else if ((lexem(std::string(1, input[i])).priority() < 0 && lexem(std::string(1, input[i + 1])).priority() > 0) || (lexem(std::string(1, input[i])).priority() < 0 && i + 1 == input.length() ))
+			{
+				lexem temp(std::string(1, input[i]));
+				temp.type = 2;
+				output.push(temp);
+				current_string.s_value.clear();
+				current_string.s_value.append(&input[i + 1], 1);
+			}
+			// initialization
+			else if (lexem(std::string(1, input[i])).priority() < 0 && std::string(1, input[i + 1]) == "=")
+			{
+				int j = i + 2;
+				while (input[j + 1] != ';')
+				{
+					j++;
+				}
+				lexem temp(input.substr(i, j - i + 1));
+				temp.type = 3;
+				output.push(temp);
+				current_string.s_value.clear();
+				current_string.s_value.append(&input[i + 1], 1);
+				i = j + 1;
+			}
+			// cycle
+			else if (input.substr(i, 6) == "while{")
+			{
+				int j = i + 6, k = 0;
+				while (input[j + 1] != '}')
+				{
+					if (k == 0)
+					{
+						if (input[j] == '<')
+							k = 1;
+						else if (input[j] == '>')
+							k = 2;
+						else if (input[j] == '=')
+							k = 3;
+					}
+					j++;
+				}
+				lexem temp(input.substr(i + 6, j - i - 6));
+				temp.d_value = k;
+				temp.type = 4;
+				output.push(temp);
+				current_string.s_value.clear();
+				current_string.s_value.append(&input[i + 1], 1);
+				i = j + 1;
+			}
 			// lexems
-			else if (priority(current_string) > -1)
+			else if (current_string.priority() > -1)
 			{
 				output.push(current_string);
 				current_string.s_value.clear();
@@ -81,7 +177,7 @@ private:
 				{
 					current_string.s_value.append(&input[i + 1], 1);
 				}
-				else if (priority(input[i + 1]) > 0)
+				else if (lexem(input[i + 1]).priority() > 0)
 				{
 					throw(-4);
 				}
@@ -89,22 +185,22 @@ private:
 			// part of lexems
 			else
 			{
-				current_string.s_value.append(&input[i], 1);
+				current_string.s_value.append(&input[i+1], 1);
 			}
 		}
 		return output;
 	}
-	queue to_poland()
+	queue create_prefix()
 	{
-		queue input_queue = to_lexems(), output(input.length());
+		queue input_queue = postfix, output(input.length() + 1);
 		stack operators(input.length());
-		unit current(0.0);
-
+		lexem current(0.0);
+		int max_priority = -2;
 		while (!input_queue.is_empty())
 		{
 			current = input_queue.pop();
-			// integers
-			if (current.type == 0)
+			// integers or variables
+			if (current.type == 0 || current.type == 2 || current.type == 3 || current.type == 4)
 			{
 				output.push(current);
 			}
@@ -112,17 +208,20 @@ private:
 			else
 			{
 				// operators
-				if (priority(current) > 0)
+				if (current.priority() > 0)
 				{
 					// stack isnt empty
 					if (!operators.is_empty())
 					{
-						unit a = operators.pop();
+						lexem a = operators.pop();
 						operators.push(a);
-						while (priority(a) >= priority(current))
+						while (a.priority() >= current.priority())
 						{
-							output.push(current);
-							current = operators.pop();
+							a = operators.pop();
+							output.push(a);
+							if (operators.is_empty()) break;
+							a = operators.pop();
+							operators.push(a);
 						}
 						operators.push(current);
 					}
@@ -140,14 +239,17 @@ private:
 					}
 					else
 					{
-						unit a = operators.pop();
+						lexem a = operators.pop();
 						while (a.s_value != "(")
 						{
 							output.push(a);
 							a = operators.pop();
 						}
-						a = operators.pop();
-						operators.push(a);
+						if (!operators.is_empty())
+						{
+							a = operators.pop();
+							operators.push(a);
+						}
 					}
 				}
 			}
@@ -159,26 +261,173 @@ private:
 		}
 		return output;
 	}
+
 public:
-	std::string input;
-	calc(std::string _input) : input(_input) {};
-	void print_reversed()
+	preprocessor(std::string _input)
 	{
-		queue a = to_poland();
-		unit temp(0.0);
+		input = _input;
+		int error = check_brackets();
+		if (error) throw ("Brackets error: " + error);
+		unary_minus_to_binary();
+		postfix = create_postfix();
+		prefix = create_prefix();
+	}
+	queue get_postfix()
+	{
+		return postfix;
+	}
+	queue get_prefix()
+	{
+		return prefix;
+	}
+};
+
+class calc
+{
+	double get_result_by_custom_in(std::string input, lexem _vars[50] = nullptr, int _vars_cnt = 0)
+	{
+		queue a = preprocessor(input).get_prefix();
+		stack temp(input.length());
+		lexem b, c, d;
+		lexem vars[50];
+		if (_vars != nullptr)
+		{
+			for (int i = 0; i < 50; i++)
+			{
+				vars[i] = _vars[i];
+			}
+		}
+		int vars_cnt = _vars_cnt;
+		bool f = true;
 		while (!a.is_empty())
 		{
-			temp = a.pop();
-			if (temp.type == 0)
-				std::cout << temp.d_value << " ";
+			f = true;
+			b = a.pop();
+			if (b.type == 0)
+			{
+				temp.push(b);
+			}
+			else if (b.type == 4)
+			{
+				lexem l, r;
+				int i = 0, j = 0;
+				while (b.s_value[i + 1] != ';')
+				{
+					if (b.s_value[i + 1] == '<' || b.s_value[i + 1] == '>' || b.s_value[i + 1] == '=')
+					{
+						l = lexem(b.s_value.substr(0, i + 1));
+						j = i + 2;
+					}
+					i++;
+				}
+				r = lexem(b.s_value.substr(j, i - j + 1));
+				for (int i = 0; i < vars_cnt; i++)
+				{
+					if (vars[i].s_value[0] == l.s_value[0])
+					{
+						l.d_value = vars[i].d_value;
+						continue;
+					}
+					if (vars[i].s_value[0] == r.s_value[0])
+					{
+						r.d_value = vars[i].d_value;
+						continue;
+					}
+				}
+				if (b.d_value == 1)
+				{
+					while (l.d_value < r.d_value)
+					{
+						
+					}
+				}
+				else if (b.d_value == 2)
+				{
+					while (l.d_value > r.d_value)
+					{
+						
+					}
+				}
+				else if (b.d_value == 3)
+				{
+					while (l.d_value == r.d_value)
+					{
+
+					}
+				}
+			}
+			else if (b.type == 3)
+			{
+				double r = get_result_by_custom_in(b.s_value.substr(2,b.s_value.length() - 2), vars, vars_cnt);
+				temp.push(r);
+				vars[vars_cnt] = lexem(b.s_value[0]);
+				vars[vars_cnt].d_value = r;
+				vars[vars_cnt].s_value = b.s_value[0];
+				vars[vars_cnt].type = 2;
+				vars_cnt++;
+			}
+			else if (b.type == 2)
+			{
+				for (int i = 0; i < vars_cnt; i++)
+				{
+					if (vars[i].s_value[0] == b.s_value[0])
+					{
+						temp.push(vars[i].d_value);
+						f = false;
+						break;
+					}
+				}
+				if (f)
+				{
+					std::cout << "Enter variable " << b.s_value[0] << ":" << std::endl << "> ";
+					std::cin >> vars[vars_cnt].d_value;
+					temp.push(vars[vars_cnt].d_value);
+					vars[vars_cnt].s_value[0] = b.s_value[0];
+					vars_cnt++;
+					f = false;
+				}
+			}
 			else
 			{
-				for (int i = 0; i < temp.s_value.length(); i++)
-					std::cout << temp.s_value[i];
-				std::cout << " ";
+				if (b.s_value == "+")
+				{
+					c = temp.pop();
+					d = temp.pop();
+					temp.push(d.d_value + c.d_value);
+				}
+				else if (b.s_value == "-")
+				{
+					c = temp.pop();
+					d = temp.pop();
+					temp.push(d.d_value - c.d_value);
+				}
+				else if (b.s_value == "*")
+				{
+					c = temp.pop();
+					d = temp.pop();
+					temp.push(d.d_value * c.d_value);
+				}
+				else if (b.s_value == "/")
+				{
+					c = temp.pop();
+					d = temp.pop();
+					temp.push(d.d_value / c.d_value);
+				}
+				else if (b.s_value == "sin")
+				{
+					c = temp.pop();
+					temp.push(sin(c.d_value));
+				}
 			}
-			std::cout << std::endl;
 		}
+		return temp.pop().d_value;
+	}
+public:
+	std::string input;
+	calc(std::string _input) : input(_input) {}
+	double get_result()
+	{
+		return get_result_by_custom_in(input);
 	}
 };
 #endif
