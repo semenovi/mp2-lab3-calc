cc8d59d86b5967234a518856e856b25c177c2144
semenovi <t79601712352@gmail.com>
2018-11-21 09:41:11 +0300

    Update container.h

--- container.h
+++ container.h
@@ -1,23 +1,41 @@
 #ifndef CONTAINER
 #define CONTAINER
 #include <iostream>
-class unit
+class lexem
 {
 public:
-	int type; //0 - double, 1 - string
-	unit() : d_value(0), s_value(""), type(0) {}
-	unit(double v) : d_value(v), s_value(""), type(0) {}
-	unit(std::string v) : d_value(0), s_value(v), type(1) {}
+	int type; //0 - double, 1 - string, 2 - variable, 3 - init, 4 - cycle
+	lexem() : d_value(0), s_value(""), type(0) {}
+	lexem(double v) : d_value(v), s_value(""), type(0) {}
+	lexem(std::string v) : d_value(0), s_value(v), type(1) {}
 	double d_value;
 	std::string s_value;
-	bool operator!=(const unit &v) const
+	int priority()
+	{
+		if (s_value == "+")
+			return 1;
+		else if (s_value == "-")
+			return 1;
+		else if (s_value == "*")
+			return 2;
+		else if (s_value == "/")
+			return 2;
+		else if (s_value == "(")
+			return 0;
+		else if (s_value == ")")
+			return 0;
+		else if (s_value == "sin")
+			return 3;
+		return -1;
+	}
+	bool operator!=(const lexem &v) const
 	{
 		if (type == 0)
 			return d_value != v.d_value;
 		else
 			return s_value != v.s_value;
 	}
-	unit operator=(const unit &s)
+	lexem operator=(const lexem &s)
 	{
 		type = s.type;
 		d_value = s.d_value;
@@ -28,16 +46,16 @@ public:
 class stack
 {
 	int size, index;
-	unit * memory;
+	lexem * memory;
 	stack() {};
 public:
 	stack(int _size) : size(_size), index(-1)
 	{
-		memory = new unit[size];
+		memory = new lexem[size];
 	}
 	stack(const stack & s) : size(s.size), index(s.index)
 	{
-		memory = new unit[size];
+		memory = new lexem[size];
 		for (int i = 0; i <= s.index; i++)
 			memory[i] = s.memory[i];
 	}
@@ -65,13 +83,13 @@ public:
 	}
 	bool is_full() { return (index == size - 1); }
 	bool is_empty() { return (index == -1); }
-	void push(unit v)
+	void push(lexem v)
 	{
 		if (is_full()) throw(-1);
 		index++;
 		memory[index] = v;
 	}
-	unit pop()
+	lexem pop()
 	{
 		if (is_empty()) throw(-2);
 		index--;
@@ -85,15 +103,15 @@ public:
 class queue
 {
 	int size, top, end;
-	unit * memory;
+	lexem * memory;
 public:
 	queue(int _size = 20) : size(_size), end(size - 1), top(0)
 	{
-		memory = new unit[size];
+		memory = new lexem[size];
 	}
 	queue(const queue &b) : size(b.size), end(b.end), top(b.top)
 	{
-		memory = new unit[size];
+		memory = new lexem[size];
 		if (is_empty()) return;
 		int i = top;
 		while (i != end + 1)
@@ -110,16 +128,16 @@ public:
 	{
 		return ((end + 2) % size == top);
 	}
-	void push(unit a)
+	void push(lexem a)
 	{
 		if (is_full()) throw(-1);
 		end = (end + 1) % size;
 		memory[end] = a;
 	}
-	unit pop()
+	lexem pop()
 	{
 		if (is_empty()) throw(-1);
-		unit k = memory[top];
+		lexem k = memory[top];
 		top = (top + 1) % size;
 		return k;
 	}
